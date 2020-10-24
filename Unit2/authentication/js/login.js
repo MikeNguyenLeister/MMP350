321916a3cffc0899207e17462f5e04de3618af5f
MikeNguyenLeister <57691720+MikeNguyenLeister@users.noreply.github.com>
2020-10-23 23:22:42 -0400

    Update login.js

--- Unit2/authentication/js/login.js
+++ Unit2/authentication/js/login.js
@@ -1,7 +1,7 @@
 const loginButton = document.getElementById('submit-login');
 const loginEmail = document.getElementById('login-email');
 const loginPassword = document.getElementById('login-password');
-loginButton.onclick = function(){
+loginButton.onclick = function() {
     const email = loginEmail.value;
     const password = loginPassword.value;
     //firebase.auth().signInWithEmailAndPassword(email,password);
