7eb5dc10ea6a4654748284f4e69ca6f86b98f4ad
MikeNguyenLeister <57691720+MikeNguyenLeister@users.noreply.github.com>
2020-10-23 23:17:56 -0400

    login

---
+++ Unit2/authentication/js/login.js
@@ -0,0 +1,15 @@
+const loginButton = document.getElementById('submit-login');
+const loginEmail = document.getElementById('login-email');
+const loginPassword = document.getElementById('login-password');
+loginButton.onclick = function(){
+    const email = loginEmail.value;
+    const password = loginPassword.value;
+    //firebase.auth().signInWithEmailAndPassword(email,password);
+    const promise = firebase.auth().signInWithEmailAndPassword(email,password);
+    
+    //error message
+    promise.catch(function(error){
+        console.log(error);
+        alert(error.message);
+  });           
+};
