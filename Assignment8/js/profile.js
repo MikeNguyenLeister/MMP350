6c0af2d0e7e0875253806ccd147fe31d31f813cb
MikeNguyenLeister <57691720+MikeNguyenLeister@users.noreply.github.com>
2020-12-21 02:34:18 -0500

    Update profile.js

--- Assignment8/js/profile.js
+++ Assignment8/js/profile.js
@@ -1,4 +1,36 @@
-  
+/ get user profile id
+const profileUID = location.search.split('=')[1];
+fb.getUserProfile(profileUID);
+
+const profileName = js.getEl("profile-display-name");
+const profileBio = js.getEl("profile-bio");
+const profileButton = js.getEl("update-profile");
+
+function displayProfile(userName, userInfo) {
+
+	// display the user profile name
+	profileName.value = userName;
+
+	// add other other fields
+
+	if (userInfo.bio) {
+		profileBio.value = userInfo.bio;
+	}
+
+}
+
+profileButton.onclick = function() {
+	// helper: user id, field name 
+	fb.updateProfile(profileUID, 'displayName', profileName.value);
+	fb.updateProfile(profileUID, 'bio', profileBio.value);
+};
+
+
+
+
+
+
+/*  
 // get user profile id
 const profileUID = location.search.split('=')[1];
 fb.getUserProfile(profileUID);
@@ -47,4 +79,4 @@ function addProfileImage(imageURL) {
     profileImage.src = imageURL;
     fb.updateProfile(profileUID, 'imageURL', imageURL);
 }
-
+*/
